f1c3a8236b4e0a2da5a0d8a93ea49493411839d4
Ana <airlukasz89@gmail.com>
2022-11-14 21:23:02 +0100

    Init

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,14 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
+export default class MainScene extends Phaser.Scene
 {
     private player : Phaser.GameObjects.Image;
+    private tankIdleAudio : Phaser.Sound.BaseSound;
+    private tankMovingAudio : Phaser.Sound.BaseSound;
+    private cursors : Phaser.Types.Input.Keyboard.CursorKeys;
+    private layer : Phaser.Tilemaps.TilemapLayer;
+
+    private speed : number = 4;
 
     constructor ()
     {
@@ -24,8 +30,59 @@ export default class Demo extends Phaser.Scene
         return false;
     }
 
+    moveTank(vector : Phaser.Math.Vector2, angle : number, tileOffset : Phaser.Math.Vector2){
+        console.log(tileOffset);
+        
+        var tile = this.layer.getTileAtWorldXY(tileOffset.x , tileOffset.y, true);
+
+        if (tile.index === 2)
+        {
+            console.log("2");
+            this.tankMovingAudio.stop();
+            //  Blocked, we can't move
+        }
+        else
+        {
+            if(!this.tankMovingAudio.isPlaying) this.tankMovingAudio.play();
+            this.player.x = vector.x;
+            this.player.y = vector.y;
+            this.player.angle = angle;
+        }
+
+    }
+
+    moveTankAllDirections()
+    {
+        if(this.cursors.left.isDown)
+        {
+            let moveVector = new Phaser.Math.Vector2(this.player.x - this.speed, this.player.y);
+            let tileOffset = new Phaser.Math.Vector2(this.player.x - 16 - this.speed, this.player.y);
+            this.moveTank(moveVector, 180, tileOffset);
+        }
+        if(this.cursors.right.isDown)
+        {
+            let moveVector = new Phaser.Math.Vector2(this.player.x + this.speed, this.player.y);
+            let tileOffset = new Phaser.Math.Vector2(this.player.x + 16 , this.player.y);
+            this.moveTank(moveVector, 0, tileOffset);
+        }
+        if(this.cursors.up.isDown)
+        {
+            let moveVector = new Phaser.Math.Vector2(this.player.x, this.player.y - this.speed);
+            let tileOffset = new Phaser.Math.Vector2(this.player.x, this.player.y - 16 - this.speed);
+            this.moveTank(moveVector, -90, tileOffset);
+        }
+        if(this.cursors.down.isDown)
+        {
+            let moveVector = new Phaser.Math.Vector2(this.player.x, this.player.y + this.speed);
+            let tileOffset = new Phaser.Math.Vector2(this.player.x, this.player.y + 16);
+            this.moveTank(moveVector, 90, tileOffset);
+        }
+    }
+
     update () 
     {
+        this.moveTankAllDirections();
+
         let coliding = this.isColiding(this.player.x, this.player.y, 32, 32,
                 112, 48, 32, 32);
 
@@ -35,129 +92,48 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
+        this.load.audio('tank_idle', ['assets/tank_idle.mp3']);
+        this.load.audio('tank_moving', ['assets/tank_moving.mp3']);
+
 
         this.load.image('tiles', 'assets/drawtiles-spaced.png');
         this.load.image('car', 'assets/car90.png');
         this.load.tilemapCSV('map', 'assets/grid.csv');
 
-        // this.load.image('logo', 'assets/phaser3-logo.png');
-        // this.load.image('libs', 'assets/libs.png');
-        // this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        // this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
     create ()
     {
-        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        // this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        // this.add.image(400, 300, 'libs');
-
-        // const logo = this.add.image(400, 70, 'logo');
-
-        // this.tweens.add({
-        //     targets: logo,
-        //     y: 350,
-        //     duration: 1500,
-        //     ease: 'Bounce.InOut',
-        //     yoyo: true,
-        //     repeat: -1
-        // })
-
+        this.cursors = this.input.keyboard.createCursorKeys();
+        
+        this.tankIdleAudio = this.sound.add("tank_idle");
+        this.tankMovingAudio = this.sound.add("tank_moving", {
+            loop: true
+        });
         
         var map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
         var tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2);
-        var layer = map.createLayer(0, tileset, 0, 0);
+        this.layer = map.createLayer(0, tileset, 0, 0);
 
         this.player = this.add.image(32+16, 32+16, 'car');
         let player = this.player;
 
-        let speed = 4;
+        let tankMovingAudio = this.tankMovingAudio;
 
-        //  Left
-        this.input.keyboard.on('keydown-A', function (event) {
-
-
-            var tile = layer.getTileAtWorldXY(player.x - 16 - speed, player.y, true);
-
-            if (tile.index === 2)
-            {
-                //  Blocked, we can't move
-            }
-            else
-            {
-                player.x -= speed;
-                player.angle = 180;
-            }
-
-        });
-
-        //  Right
-        this.input.keyboard.on('keydown-D', function (event) {
-
-            var tile = layer.getTileAtWorldXY(player.x + 16 , player.y, true);
-
-            if (tile.index === 2)
-            {
-                //  Blocked, we can't move
-            }
-            else
-            {
-                player.x += speed;
-                player.angle = 0;
-            }
-
-        });
-
-        //  Up
-        this.input.keyboard.on('keydown-W', function (event) {
-
-            var tile = layer.getTileAtWorldXY(player.x, player.y - 16 - speed , true);
-
-            if (tile.index === 2)
-            {
-                //  Blocked, we can't move
-            }
-            else
-            {
-                player.y -= speed;
-                player.angle = -90;
-            }
-
-        });
-
-        //  Down
-        this.input.keyboard.on('keydown-S', function (event) {
-
-            var tile = layer.getTileAtWorldXY(player.x, player.y + 16 , true);
-
-            if (tile.index === 2)
-            {
-                //  Blocked, we can't move
-            }
-            else
-            {
-                player.y += speed;
-                player.angle = 90;
-            }
-
+        this.input.keyboard.on("keyup", function(){
+            tankMovingAudio.stop();
         });
     }
 }
 
 const config = {
-    // type: Phaser.AUTO,
-    // backgroundColor: '#125555',
-    // width: 800,
-    // height: 600,
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     parent: 'phaser-example',
     pixelArt: true,
     backgroundColor: '#1a1a2d',
-    scene: Demo
+    scene: MainScene
 };
 
 const game = new Phaser.Game(config);
